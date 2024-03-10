f30bbbf5b136b8f1ac04a8638c48a03890d06ac5
anyars-encarta <anyarsencarta@gmail.com>
2024-03-10 11:03:39 +0000

    Complete backend

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const blogRouter = require('./route/blog-route');
 
 require('./db/db');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/blogs', blogRouter);
 
 app.use('/api', (req, res) => {
     res.send('Hello Encarta');
