d4058855cb0a0056f6fc5a9316f479d1fc469c1c
anyars-encarta <anyarsencarta@gmail.com>
2024-03-11 09:46:50 +0000

    Add field values

--- client/src/context/context.jsx
+++ client/src/context/context.jsx
@@ -1,10 +1,12 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
 export const GlobalContext = createContext(null);
 
 const GlobalState = ({ children }) => {
+    const [formData, setFormData] = useState({title: '', description: ''});
+
     return(
-        <GlobalContext.Provider>
+        <GlobalContext.Provider value={{formData, setFormData}}>
             {children}
         </GlobalContext.Provider>
     )
--- client/src/pages/add-blog/addBlog.jsx
+++ client/src/pages/add-blog/addBlog.jsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
 import classes from './addBlog.module.css';
+import { GlobalContext } from '../../context/context';
 
 const AddBlog = () => {
+    const { formData, setFormData } = useContext(GlobalContext);
+    console.log(formData);
     return (
         <div className={classes.wrapper}>
             <h1>Add a New Blog</h1>
@@ -10,12 +14,16 @@ const AddBlog = () => {
                     placeholder='Enter Blog Title'
                     id='title'
                     type='text'
+                    value={formData.title}
+                    onChange={(e) => setFormData({ ...formData, title: e.target.value })}
                 />
 
-                <textarea 
-                name='description'
-                placeholder='Enter Blog description'
-                id='description'
+                <textarea
+                    name='description'
+                    placeholder='Enter Blog description'
+                    id='description'
+                    value={formData.description}
+                    onChange={(e) => setFormData({ ...formData, description: e.target.value })}
                 />
 
                 <button>Add New Blog</button>
