f30dcd60f7b667095e409a4615e4680dfaae6296
anyars-encarta <anyarsencarta@gmail.com>
2024-03-11 09:33:08 +0000

    Add form elements

--- client/src/pages/add-blog/addBlog.jsx
+++ client/src/pages/add-blog/addBlog.jsx
@@ -1,7 +1,25 @@
+import classes from './addBlog.module.css';
+
 const AddBlog = () => {
     return (
-        <div>
+        <div className={classes.wrapper}>
             <h1>Add a New Blog</h1>
+            <div className={classes.formWrapper}>
+                <input
+                    name='title'
+                    placeholder='Enter Blog Title'
+                    id='title'
+                    type='text'
+                />
+
+                <textarea 
+                name='description'
+                placeholder='Enter Blog description'
+                id='description'
+                />
+
+                <button>Add New Blog</button>
+            </div>
         </div>
     )
 };
