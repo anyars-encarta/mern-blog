d28dd11879f62fcfa2689d65dbc67cd40f0ebf92
anyars-encarta <anyarsencarta@gmail.com>
2024-03-31 08:20:36 +0000

    Add vercel.json configurations

--- server/db/db.js
+++ server/db/db.js
@@ -4,7 +4,7 @@ const mongoose =  require('mongoose');
 mongoose.set('strictQuery', false);
 
 mongoose.connect(
-    `mongodb+srv://anyarsencarta:${process.env.Mongo_Db_Password}@cluster0.z1yd29e.mongodb.net/`
+    `mongodb+srv://anyarsencarta:${process.env.Mongo_Db_Password}@cluster0.z1yd29e.mongodb.net/?retryWrites=true&w=majority`
     )
     .then(() => console.log('Connected to my Mongo Db'))
     .catch((e) => console.log(e));
